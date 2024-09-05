b8e1e5f9adf189fc920af3bd785dc2edbacd5023
gedeontimothy <gedeonbateko3@gmail.com>
2024-09-05 05:06:24 +0100

    [UPDATE] Config | Root Template - Nouvelle config de tailwind et ajoute du fichier `resources/css/app.css` sur la directive `@vite` dans le fichier de template root

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}",],
+  content: [
+	'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+	'./storage/framework/views/*.php',
+	'./resources/views/**/*.blade.php',
+	'./resources/js/**/*.vue',
+  ],
   theme: {
-    extend: {},
+	extend: {},
   },
   plugins: [],
 }
