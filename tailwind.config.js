26bc533be6fddaf1ed6384e903915b49c8bd0eaf
gedeontimothy <gedeonbateko3@gmail.com>
2024-09-05 23:12:47 +0100

    [ADD|UPDATE] Package - Ajout et configuration du package `flowbite-vue` avec la CLI `npm i flowbite flowbite-vue`

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,14 @@ export default {
 	'./storage/framework/views/*.php',
 	'./resources/views/**/*.blade.php',
 	'./resources/js/**/*.vue',
+  'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
+  'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
 	extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
